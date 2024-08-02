4d90628edc1a0ff4d8837fc61cc89c9e37b0debf
raj-sarade23 <elaunch.rajashri@gmail.com>
2024-08-02 16:55:09 +0530

    visa

--- src/main/java/com/example/tour_ms/visa.java
+++ src/main/java/com/example/tour_ms/visa.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class visa {
 
-    @GetMapping("/flight")
+    @GetMapping("/visa")
 
     public String getData() {
         return "Please book visa in 20% discount.VISA-----..";
