442b2a23f29f0cfab3cf0866b6eee9caef5eeb9e
raj-sarade23 <elaunch.rajashri@gmail.com>
2024-08-02 16:45:50 +0530

    flight

---
+++ src/main/java/com/example/tour_ms/flight.java
@@ -0,0 +1,15 @@
+package com.example.tour_ms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class flight {
+
+    @GetMapping("/flight")
+
+    public String getData() {
+        return "Please book flight in 20% discount.grab it DEV branch webhook plugin -jenkinsghfghffghg..";
+    }
+
+}
+
