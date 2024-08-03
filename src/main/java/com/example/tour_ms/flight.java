f856a73f43b55490eb3d131a25a48dbbd7377ecc
raj-sarade23 <elaunch.rajashri@gmail.com>
2024-08-03 22:47:42 +0530

    chnages

--- src/main/java/com/example/tour_ms/flight.java
+++ src/main/java/com/example/tour_ms/flight.java
@@ -8,7 +8,7 @@ public class flight {
     @GetMapping("/flight")
 
     public String getData() {
-        return "Please book flight in 20% discount.grab it DEV branch webhook plugin -jenkinsghfghffghg..";
+        return "Please book flight in 20% discount.grab it DEV branch webhook plugin -jenkins.";
     }
 
 }
